4e4af44e4638e2874c05543cf731c23862e17bb2
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-05-14 20:10:51 +0200

    fix(fresaprice): precio pastel de fresa corregido a 20

--- pasteleria/Main.java
+++ pasteleria/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // Create the order
         Pedido pedido = new Pedido();
-        pedido.addPastel(new PastelVainilla());
         pedido.addPastel(new PastelChocolate());
+        pedido.addPastel(new Frase(new PastelVainilla(),"”¡básico!”"));
+        pedido.addPastel(new Frase(new Fideos(new PastelVainilla()),"”¡sofisticado!”"));
+        pedido.addPastel(new Frase(new Frase(new Fideos(new Fideos(new Multicapa(new PastelFresa()))),"”el doble de”"),"”todo”"));
 
         // Print the order
         pedido.printPedido();
--- pasteleria/PastelFresa.java
+++ pasteleria/PastelFresa.java
@@ -1,7 +1,14 @@
 package com.montecastelo.pasteleria;
 
 public class PastelFresa extends Pastel{
+
+    private final int PRECIO = 20;
+
     public String getDescripcion() {
         return "Pastel de fresa";
+    }
+    @Override
+    public int getPrecio(){
+        return PRECIO;
     }
 }
